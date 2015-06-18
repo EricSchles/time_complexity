0124056ba3e1680ad7af3bace120050fc67780e6
EricSchles <ericschles@gmail.com>
2015-06-18 07:24:02 -0700

    "Updating anything that was missing"

--- analyzer.py
+++ analyzer.py
@@ -4,7 +4,8 @@ def read_code():
     filename = argv[1]
     with open(filename,"r") as f: return f.read().split("\n")
 
-def test_runtime():
+def test_runtime(code):
+    counter = 0
     for ind,line in enumerate(code):
         #conditions that would imply O(n)
         conditions = [
@@ -18,7 +19,7 @@ def test_runtime():
         for condition in conditions:
             if all([elem in line for elem in condition]):
                 if all([elem in code[ind+1] for elem in condition]): big_o_squared = True
-
+                
         if any([re.search(elem,line) for elem in anti_conditions]):
             continue
         print "O(n^2) complexity found on line:",ind
@@ -27,11 +28,17 @@ def test_runtime():
             continue
         print "O(n) complexity found on line:",ind
         break
-    
+
+
+#def complete_runtime(code,ind,conditions,counter=0):
+#    for condition in conditions:
+        
+
+#covers the case many for loops wrapped together
 def poly_runtime(code,ind,conditions,counter=0):
     for condition in conditions:
         if not all([elem in code[ind] for elem in condition]):
-            return "O(n^"+str(counter)+") complexity found on line:",ind
+            return "O(n^"+str(counter)+") complexity found on line:",ind+1
         else:
             return poly_runtime(code,ind+1,conditions,counter+1)
 
@@ -52,7 +59,7 @@ def run():
         ["for","in","[","]"]
     ]
 
-    ind = 0 
+    ind = 0
     while ind < len(code)-1:
         result,ind = poly_runtime(code,ind,conditions)
         print result, ind
